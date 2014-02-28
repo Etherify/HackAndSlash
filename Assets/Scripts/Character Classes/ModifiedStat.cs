ee0d2c5b64f79773a0577171124a99c018ccb4a2
Namuras <Bigbadtim52@hotmail.com>
2014-02-28 18:50:27 -0500

    Initial Check In
    Initial Check in

---
+++ Assets/Scripts/Character Classes/ModifiedStat.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+
+public class ModifiedStat : BaseStat
+{
+
+    private List<ModfiyingAttribute> _mods;  // list of attributes that modify this stat
+    private int _modValue;                   // amount added to the baseValue from the modifiers
+
+
+
+    public ModifiedStat()
+    {
+
+        _mods = new List<ModfiyingAttribute>();
+        _modValue = 0;
+
+    }
+
+    public void AddModifier(ModfiyingAttribute mod)
+    {
+        _mods.Add(mod);
+    }
+
+    private void CalculateModValue()
+    {
+        _modValue = 0;
+
+        if (_mods.Count > 0)
+            foreach (ModfiyingAttribute att in _mods)
+                _modValue += (int)(att.attribute.AdjustedBaseValue * att.ratio);
+    }
+
+    
+    public new int AdjustedBaseValue
+    {
+        get { return BaseValue + BuffValue + _modValue; }
+    }
+
+    public void Update()
+    {
+        CalculateModValue();
+    }
+
+    
+    
+    public struct ModfiyingAttribute
+    {
+        public Attribute attribute;
+        public float ratio;
+    }
+
+
+
+}
---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAttack : MonoBehaviour {
+
+    public GameObject target;
+    public float attackTimer;
+    public float coolDown;
+
+    
+    
+    // Use this for initialization
+	void Start () {
+
+        attackTimer = 0;
+        coolDown = 2.0f;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+
+        //every frame the attack time is >0 it gets reduced by the time it to render the frame. 
+        if (attackTimer > 0)
+            attackTimer -= Time.deltaTime;
+
+
+        if (attackTimer < 0)
+            attackTimer = 0;
+
+        if (Input.GetKeyUp(KeyCode.F))
+        {
+            if (attackTimer == 0)
+            {
+                Attack();
+                attackTimer = coolDown;
+            }
+        }
+
+        
+	}
+
+
+    private void Attack()
+    {
+
+        float distance = Vector3.Distance(target.transform.position,transform.position);
+
+        //set direction and move one unit forward
+        Vector3 dir = (target.transform.position - transform.position).normalized;
+        float direction = Vector3.Dot(dir, transform.forward);
+
+        //call new instance of Enemny health
+        EnemyHealth eh = (EnemyHealth)target.GetComponent("EnemyHealth");
+
+        Debug.Log(distance);
+
+        //should return between 1 and -1, 1 is in front, -1 is behind
+
+        Debug.Log(direction);
+
+
+        if (distance <= 2.5f)
+        {
+            if (direction > 0)
+            {
+                eh.AdjustCurrentHealth(-10);
+            }
+        }
+
+    }
+
+
+}
