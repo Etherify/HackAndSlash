666b2b0c85367eba0c95e302b50ef714a4c98a94
Namuras <Bigbadtim52@hotmail.com>
2014-03-02 09:15:41 -0500

    Through Ep 25
    Through 25 episodes of BurgZergArcade Hack and Slash demo

---
+++ Assets/Scripts/Character Classes/BaseCharacter.cs
@@ -0,0 +1,183 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;  //added to access enum class
+
+
+public class BaseCharacter : MonoBehaviour {
+
+    private string _name;
+    private int _level;
+    private uint _freeExp;
+
+    //Attribute att = new Attribute();
+
+
+    private Attribute[] _primaryAttribute;
+    private Vital[] _vital;
+    private Skill[] _skill;
+
+
+    public void Awake()
+    {
+        _name = String.Empty;
+        _level = 0;
+        _freeExp = 0;
+
+        _primaryAttribute = new Attribute[Enum.GetValues(typeof(AttributeName)).Length];
+		_vital = new Vital[Enum.GetValues(typeof(VitalName)).Length];
+        _skill = new Skill[Enum.GetValues(typeof(SkillName)).Length];
+
+        SetupPrimaryAttributes();
+        SetupVitals();
+		SetupSkills ();
+
+    }
+
+
+    public string Name
+    {
+        get {return _name;}
+        set {_name = value;}
+
+    }
+
+    public int level 
+    {
+        get{ return _level; }
+        set{ _level = value; }
+    }
+
+    public uint FreeExp
+    {
+        get{return _freeExp;}
+        set{_freeExp = value;}
+    }
+
+    private void SetupPrimaryAttributes()
+    {
+        for (int cnt = 0; cnt < _primaryAttribute.Length; cnt++)
+            _primaryAttribute[cnt] = new Attribute();      
+    }
+
+    private void SetupVitals()
+    {
+        for (int cnt = 0; cnt < _vital.Length; cnt++)
+            _vital[cnt] = new Vital();
+
+        SetupVitalModifiers();
+
+    }
+
+    private void SetupSkills()
+    {
+        for (int cnt = 0; cnt < _skill.Length; cnt++)
+            _skill[cnt] = new Skill();
+
+        SetupSkillModifiers();
+    }
+
+    //Take Average of players skills and assing that as player level
+    public void CalculateLevel()
+    {
+
+    }
+
+
+    public void AddExp(uint exp)
+    {
+        _freeExp += exp;
+
+
+        CalculateLevel(); 
+    }
+
+    public Attribute GetPrimaryAttribute(int index)
+    {
+        return _primaryAttribute[index];
+    }
+
+    public Vital GetVital(int index)
+    {
+        return _vital[index];
+    }
+
+    public Skill GetSkill(int index)
+    {
+        return _skill[index];
+    }
+
+    private void SetupVitalModifiers()
+    {
+        ////health
+        //ModifyingAttribute health = new ModifyingAttribute();
+        //health.attribute = GetPrimaryAttribute((int)AttributeName.Constitution);
+        //health.ratio = .5f;
+
+        //GetVital((int)VitalName.Health).AddModifier(health);
+
+        
+        ////mana
+        //ModifyingAttribute mana = new ModifyingAttribute();
+        //mana.attribute = GetPrimaryAttribute((int)AttributeName.Intelligence);
+        //mana.ratio = 1f;
+
+        //GetVital((int)VitalName.Mana).AddModifier(mana);
+        
+        ////energy
+        //ModifyingAttribute energy = new ModifyingAttribute();
+        //energy.attribute = GetPrimaryAttribute((int)AttributeName.Charisma);
+        //energy.ratio = .5f;
+
+        //GetVital((int)VitalName.Energy).AddModifier(energy);
+
+
+        //not needed code due to adding code into Get:  lines
+        //ModifiedStat.ModfiyingAttribute health = new ModifiedStat.ModfiyingAttribute();
+        //health.attribute = GetPrimaryAttribute((int)Attribute.AttributeName.Constitution);
+        //health.ratio = .5f;
+
+
+        GetVital((int)VitalName.Health).AddModifier(new ModifyingAttribute (GetPrimaryAttribute((int)AttributeName.Constitution), .5f));
+        GetVital((int)VitalName.Mana).AddModifier(new ModifyingAttribute  (GetPrimaryAttribute((int)AttributeName.Willpower), 1f ));
+        GetVital((int)VitalName.Energy).AddModifier(new ModifyingAttribute  (GetPrimaryAttribute((int)AttributeName.Nimbleness), 1f ));
+
+
+
+
+    }
+
+	private void SetupSkillModifiers() 
+    {
+
+        //Melee Offense
+        GetSkill((int)SkillName.Melee_Offense).AddModifier(new ModifyingAttribute(GetPrimaryAttribute((int)AttributeName.Might), .33f));
+        GetSkill((int)SkillName.Melee_Offense).AddModifier(new ModifyingAttribute(GetPrimaryAttribute((int)AttributeName.Nimbleness), .33f));
+        //Melee Defense
+        GetSkill((int)SkillName.Melee_Defense).AddModifier(new ModifyingAttribute(GetPrimaryAttribute((int)AttributeName.Speed), .33f));
+        GetSkill((int)SkillName.Melee_Defense).AddModifier(new ModifyingAttribute(GetPrimaryAttribute((int)AttributeName.Constitution), .33f));
+
+        //Magic Offense
+        GetSkill((int)SkillName.Magic_Offense).AddModifier(new ModifyingAttribute(GetPrimaryAttribute((int)AttributeName.Willpower), .33f));
+        GetSkill((int)SkillName.Magic_Offense).AddModifier(new ModifyingAttribute(GetPrimaryAttribute((int)AttributeName.Concentration), .33f));
+        //Magic Defense
+        GetSkill((int)SkillName.Magic_Defense).AddModifier(new ModifyingAttribute(GetPrimaryAttribute((int)AttributeName.Willpower), .33f));
+        GetSkill((int)SkillName.Magic_Defense).AddModifier(new ModifyingAttribute(GetPrimaryAttribute((int)AttributeName.Concentration), .33f));
+		
+        //Ranged Offense
+        GetSkill((int)SkillName.Ranged_Offense).AddModifier(new ModifyingAttribute(GetPrimaryAttribute((int)AttributeName.Concentration), .33f));
+        GetSkill((int)SkillName.Ranged_Offense).AddModifier(new ModifyingAttribute(GetPrimaryAttribute((int)AttributeName.Speed), .33f));
+        //Ranged Defense
+        GetSkill((int)SkillName.Ranged_Defense).AddModifier(new ModifyingAttribute(GetPrimaryAttribute((int)AttributeName.Speed), .33f));
+        GetSkill((int)SkillName.Ranged_Defense).AddModifier(new ModifyingAttribute(GetPrimaryAttribute((int)AttributeName.Nimbleness), .33f));
+		
+	}
+
+    public void StatUpdate()
+    {
+        for (int cnt = 0; cnt < _vital.Length; cnt++)
+            _vital[cnt].Update();
+        for (int cnt = 0; cnt < _skill.Length; cnt++)
+            _skill[cnt].Update();
+
+    }
+}
---
+++ Assets/Scripts/Character Classes/CharacterGenerator.cs
@@ -0,0 +1,176 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class CharacterGenerator : MonoBehaviour
+{
+
+    private PlayerCharacter _toon;
+    private const int STARTING_POINTS = 350;
+    private const int MIN_STARTING_ATTRIBUTE_VALUE = 10;
+    private const int STARTINGVALUE = 50;
+    private int pointsLeft;
+
+
+    //buffer between char creation
+    private const int OFFSET = 5;
+
+    //how tall the stat lines are
+    private const int LINEHEIGHT = 20;
+
+    //how long the boxes in the Char Creation are
+    private const int STAT_LABEL_WIDTH = 100;
+    private const int BASEVALUE_LABEL_WIDTH = 30; 
+
+    //button dimensions
+
+    private const int BUTTON_WIDTH = 20;
+    private const int BUTTON_HEIGHT = 20;
+
+    
+    //starting Y position of stats
+    private int statStartingPosition = 40;
+
+    //starting x positon of points left
+    private int startingStatPosition = 250;
+
+
+
+
+
+    
+
+    // Use this for initialization
+    void Start()
+    {
+        _toon = new PlayerCharacter();
+        _toon.Awake();
+
+        pointsLeft = STARTING_POINTS;
+
+        for (int cnt = 0; cnt < Enum.GetValues(typeof(AttributeName)).Length; cnt++)
+        {
+            //minimum point is 10, but 50 will start in each
+            _toon.GetPrimaryAttribute(cnt).BaseValue = STARTINGVALUE;
+            pointsLeft-=(STARTINGVALUE - MIN_STARTING_ATTRIBUTE_VALUE);
+
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnGUI()
+    {
+        DisplayName();
+        DisplayPointsLeft();
+        DisplayAttributes();
+        DisplayVitals();
+        DisplaySkills();
+
+    }
+
+    private void DisplayName()
+    {
+        GUI.Label(new Rect(10, 10, 50, 25), "Name");
+        _toon.Name = GUI.TextField(new Rect(65, 10, 100, 25), _toon.Name);
+
+    }
+
+	private void DisplayAttributes()
+	{
+		for (int cnt = 0; cnt < Enum.GetValues(typeof(AttributeName)).Length; cnt++)
+		{
+			GUI.Label(new Rect( OFFSET,                                     //x
+                                statStartingPosition + (cnt * LINEHEIGHT),  //y
+                                STAT_LABEL_WIDTH,                           //width
+                                LINEHEIGHT),                                //height 
+                                
+                                ((AttributeName)cnt).ToString());
+			GUI.Label(new Rect(STAT_LABEL_WIDTH + OFFSET, statStartingPosition + (cnt * LINEHEIGHT), BASEVALUE_LABEL_WIDTH, LINEHEIGHT), _toon.GetPrimaryAttribute(cnt).AdjustedBaseValue.ToString());
+
+            if (GUI.Button(new Rect(OFFSET + STAT_LABEL_WIDTH + BASEVALUE_LABEL_WIDTH, 
+                statStartingPosition + (cnt * BUTTON_HEIGHT), 
+                BUTTON_WIDTH, 
+                BUTTON_HEIGHT), 
+                "-"))
+            {
+                if(pointsLeft >0)
+                {
+                    if(_toon.GetPrimaryAttribute(cnt).BaseValue > MIN_STARTING_ATTRIBUTE_VALUE)
+                    {
+                        _toon.GetPrimaryAttribute(cnt).BaseValue--;
+                        pointsLeft++;
+                        _toon.StatUpdate();
+                    }
+
+
+                }
+            }
+            if (GUI.Button(new Rect(OFFSET + STAT_LABEL_WIDTH + BASEVALUE_LABEL_WIDTH + BUTTON_WIDTH, 
+                statStartingPosition + (cnt * BUTTON_HEIGHT), 
+                BUTTON_WIDTH, 
+                BUTTON_HEIGHT), 
+                "+"))
+            {
+                if (pointsLeft > 0)
+                {
+                    _toon.GetPrimaryAttribute(cnt).BaseValue++;
+                    pointsLeft--;
+                    _toon.StatUpdate();
+                }
+            }
+		}
+	}
+
+	private void DisplayVitals()
+	{
+		for (int cnt = 0; cnt < Enum.GetValues(typeof(VitalName)).Length; cnt++)
+		{
+            GUI.Label(new Rect(OFFSET, 
+                statStartingPosition + ((cnt +7) * LINEHEIGHT), 
+                STAT_LABEL_WIDTH, 
+                LINEHEIGHT), ((VitalName)cnt).ToString());
+
+            GUI.Label(new Rect(OFFSET + STAT_LABEL_WIDTH, 
+                statStartingPosition + ((cnt + 7) * LINEHEIGHT), 
+                STAT_LABEL_WIDTH, LINEHEIGHT), 
+                _toon.GetVital(cnt).AdjustedBaseValue.ToString());
+		}
+	}
+
+    private void DisplaySkills()
+    {
+        for (int cnt = 0; cnt < Enum.GetValues(typeof(SkillName)).Length; cnt++)
+        {
+
+            GUI.Label(new Rect((OFFSET * 2) + STAT_LABEL_WIDTH + BASEVALUE_LABEL_WIDTH + (BUTTON_WIDTH * 2), 
+                statStartingPosition + (cnt * LINEHEIGHT), 
+                STAT_LABEL_WIDTH, 
+                LINEHEIGHT), 
+                ((SkillName)cnt).ToString());
+
+            GUI.Label(new Rect((OFFSET * 2) + STAT_LABEL_WIDTH + BASEVALUE_LABEL_WIDTH + (BUTTON_WIDTH * 2) + STAT_LABEL_WIDTH, 
+                statStartingPosition + (cnt * LINEHEIGHT), 
+                STAT_LABEL_WIDTH, 
+                LINEHEIGHT), 
+                _toon.GetSkill(cnt).AdjustedBaseValue.ToString());
+
+        }
+    }
+
+    private void DisplayPointsLeft()
+    {
+        GUI.Label(new Rect((OFFSET * 2) + STAT_LABEL_WIDTH + BASEVALUE_LABEL_WIDTH + (BUTTON_WIDTH * 2), 
+            LINEHEIGHT, 
+            STAT_LABEL_WIDTH, 
+            LINEHEIGHT), 
+            "Points Left:" + pointsLeft.ToString());
+    }
+
+}
